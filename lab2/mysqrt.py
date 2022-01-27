5d7e8fa148ef03724f59eea226a265e300406e81
chenghau <chenghau@iastate.edu>
2022-01-27 11:26:06 -0600

    renew the function

--- lab2/mysqrt.py
+++ lab2/mysqrt.py
@@ -1,28 +1,51 @@
 ''' my own square root function '''
 
-def sqrt_newton(x,s=1.0,printshow=False):
+def sqrt_newton(x,s=1.0,printshow=False,kmax=100,TOL=1e-14):
 
-    kmax = 10;
+    # convert to float
+    x =1.0*x
+
+    assert(x>=0.0)
+    assert(s>0.0)
+    assert(kmax>0)
+    assert(TOL>0.0)
+
+    # if input is 0.0
+    if x==0.0:
+        return 0.0;
 
     for k in range(0,kmax):
         if printshow:
             print("Before iteration %s, s = %20.15f" % (k,s));
+        sold = s + 0.0;
         s = 0.5*(s + x/s);
+        if (abs(s-sold)/x<TOL):
+            break
 
     if printshow:
         print("After %s iterations, s = %20.15f" % (k+1,s));
 
     return s;
 
-def sqrt_secant(x,s=1.0,s_minus=0.0,printshow=False):
+def sqrt_secant(x,s=1.0,s_minus=0.0,printshow=False,kmax=100,TOL=1e-14):
 
-    kmax = 10;
+    assert(x>=0.0)
+    assert(s>0.0)
+    assert(kmax>0)
+    assert(TOL>0.0)
+
+    # if input is 0.0
+    if x==0.0:
+        return 0.0;
 
     for k in range(0,kmax):
         if printshow:
             print("Before iteration %s, s = %20.15f" % (k,s));
+        sold = s + 0.0;
         s = (x+s*s_minus)/(s+s_minus);
-        s_minus=s;
+        s_minus=sold;
+        if (abs(s-sold)/x<TOL):
+            break
 
     if printshow:
         print("After %s iterations, s = %20.15f" % (k+1,s));
