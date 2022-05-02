dfd03f2a0dae326f227cd0b8b4e71ebc1aba1153
chenghau <chenghau@iastate.edu>
2022-05-01 20:21:26 -0400

    fix bug

--- final_project/poisson/parallel/matrix.cpp
+++ final_project/poisson/parallel/matrix.cpp
@@ -536,8 +536,8 @@ void send_boundary_data2D(const int my_rank, const int comm_sz,
 template <typename T>
 std::vector<T> slice(std::vector<T> const &v, int m, int n)
 {
-  auto first = v.begin() + m;
-  auto last = v.begin() + n + 1;
+  auto first = v.cbegin() + m;
+  auto last = v.cbegin() + n + 1;
 
   std::vector<T> vec(first, last);
   return vec;
